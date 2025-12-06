8a54db56f87ce394e83bb336e94114cdfc1f8db6
meysonv <meysonvm@gmail.com>
2025-12-05 19:55:31 -0500

    Fix API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api/v1`, // ← Usar variable de entorno
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api/v1', // ← Usar variable de entorno
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
